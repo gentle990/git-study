978e9e199e2acfbc88095c5d81efd8a7dcac5bea
MaShengBo <1724785050@qq.com>
2023-04-10 19:45:34 +0800

    添加了login功能

---
+++ src/views/Login.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        login 开发
+        xxx    ————by 小马
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
